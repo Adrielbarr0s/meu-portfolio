a793296d84a78a939b1001dfe26dff929e590291
Adrielbarr0s <adrielbarr0s.dev@gmail.com>
2026-08-13 15:24:36 -0300

    feat: adicao de filtros dinamicos na secao de projetos e melhorias visuais de UI/UX

--- src/app/components/projetos/projetos.ts
+++ src/app/components/projetos/projetos.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projetos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projetos.html',
   styleUrl: './projetos.scss',
 })
-export class Projetos {}
+export class Projetos {
+  filtroAtivo: string = 'Todos';
+
+  setFiltro(categoria: string) {
+    this.filtroAtivo = categoria;
+  }
+}
 
